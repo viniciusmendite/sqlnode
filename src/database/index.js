a845fe25de452c556d9a02628fc3548875266740
Vinicius Mendite <vinicius.miff@gmail.com>
2020-04-14 16:22:38 -0300

    Criação da tabela Addresses, relacionamento da mesma com a tabela Users

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,16 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const User = require('../models/User');
+const Address = require('../models/Address');
+
 // Database connection
 const connection = new Sequelize(dbConfig);
+
+User.init(connection);
+Address.init(connection);
+
+User.associate(connection.models);
+Address.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const UserController = require('./Controllers/UserController');
+const AddressController = require('./Controllers/AddressController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json('hello world');
-});
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+routes.get('/users/:user_id/addresses', AddressController.index);
+routes.post('/users/:user_id/addresses', AddressController.store);
 
 module.exports = routes;
